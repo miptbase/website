26413bc904e33402bc0d911e6029702f0b9f4b0c
Татьяна Антоновна Черданцева <valehova.tany@gmail.com>
2021-06-15 14:52:01 +0300

    json data

--- pages/index.js
+++ pages/index.js
@@ -1,5 +1,5 @@
 import React, { Component } from 'react'
-import { attributes} from '../content/home.json';
+import { top, features, donors, goal, media, feedback} from '../content/home.json';
 
 import HomeTop from "../components/HomeTop";
 import Features from "../components/Features";
@@ -8,16 +8,25 @@ import Donors from "../components/Donors";
 import Goal from "../components/Goal";
 import Media from "../components/Media";
 import Feedback from "../components/Feedback";
-import Transfer from "../components/Transfer";
 import Page from "../components/Page";
 
 export default class Home extends Component {
     render() {
-        let {top, features, donors, goal, media, feedback, transfer} = attributes;
         return (
             <Page>
                 <>
+                    <HomeTop
+                        title={top.title}
+                        content={top.content}
+                        img={top.img}
+                    />
+                    <Features
+                        features={features}
+                    />
+                    <Goal goal={goal}/>
                     <Media media={media}/>
+                    <Donors donors={donors}/>
+                    <Donation/>
                     <Feedback feedback={feedback} />
                 </>
             </Page>
--- pages/transfer.js
+++ pages/transfer.js
@@ -1,14 +1,13 @@
 import React, { Component } from 'react'
 import Page from "../components/Page";
 import Transfer from "../components/Transfer";
-import {attributes} from '../content/home.md';
+import {transfer} from '../content/home.json';
 
 export default class TransferPage extends Component {
     render() {
-        let {transfer} = attributes;
         return (
             <Page>
-
+                <Transfer transfer={transfer}/>
             </Page>
         )
     }
