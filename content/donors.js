69335276311524695595a53e0e3652166e6f8265
Andrey Makov <one1l@yandex.ru>
2021-06-24 23:38:57 +0300

    Рефактор donors.js

--- content/donors.js
+++ content/donors.js
@@ -1,34 +1,18 @@
-const { exec } = require("child_process");
-const patch = "public/files/donors.csv"
-const cp = require('child_process');
+const { default: axios } = require('axios');
+const csv = require('csvtojson');
+const fs = require('fs');
+
+const outputPath = './content/donors.json';
 const url = 'https://docs.google.com/spreadsheets/d/1CD_sGqy6C5dpChHq2uPs-I8q3MZOkRuyS5e4tMeTHhA/gviz/tq?tqx=out:csv&sheet=donors';
-
-let download = async function(uri, filename){
-    let command = `curl -o ${filename}  '${uri}'`;
-    let result = cp.execSync(command);
-};
-
-
-async function getDonors() {
-    await download( url, patch);
-};
-
-getDonors();
-
-
-exec( `csvtojson ${patch} > content/donors.json`, (error, stdout, stderr) => {
-    if (error) {
-        console.log(`error: ${error.message}`);
-        return;
-    }
-    if (stderr) {
-        console.log(`stderr: ${stderr}`);
-        return;
-    }
-    console.log(`stdout: ${stdout}`);
-})
-
-
-
-
-
+const output = [];
+(async () => {
+  const response = await axios.get(url);
+  csv()
+    .fromString(response.data)
+    .subscribe((csvLine)=>{ 
+      output.push(csvLine);
+    })
+    .on('done', (error)=>{
+      fs.writeFileSync(outputPath, JSON.stringify(output));
+    })
+})();
