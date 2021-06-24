ebe0ea4ea1cbdae6f4fecf97bba485efa7100e80
Татьяна Антоновна Черданцева <valehova.tany@gmail.com>
2021-06-25 01:32:15 +0300

    inner page add

--- components/InnerPage/index.js
+++ components/InnerPage/index.js
@@ -1,19 +1,33 @@
-import React from 'react'
-import style from "./inner-page_.module.scss"
+import React, { Component } from 'react';
 
+class InnerPage extends Component {
+    static async getInitialProps({ query }) {
+        const { slug } = query;
+        const blogpost = await import(`../../../content/innerPage/${slug}.md`).catch(error => null);
 
-const InnerPage = (props) => {
-    const {title, children} = props;
-    return (
-        <div className={style.innerPage}>
-            <h1 className={style.title}>
-                {title}
-            </h1>
-            <div className={style.article}>
-                {children}
-            </div>
-        </div>
-    )
+        return { blogpost };
+    }
+    render() {
+        if (!this.props.blogpost) return <div>not found</div>;
+
+        const {
+            html,
+            attributes: { titlePage },
+        } = this.props.blogpost.default;
+
+        return (
+            <>
+                <article>
+                    <div className={style.innerPage}>
+                        <h1 className={style.title}>
+                            {titlePage}
+                        </h1>
+                        <div dangerouslySetInnerHTML={{ __html: html }} />
+                    </div>
+                </article>
+            </>
+        );
+    }
 }
 
-export default InnerPage
+export default InnerPage;
