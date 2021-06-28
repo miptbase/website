5c88fdaa90b8e180b57e5e5989ca6a3c59a4e6f3
Татьяна Антоновна Черданцева <valehova.tany@gmail.com>
2021-06-28 10:34:08 +0300

    remove identify script

--- components/Page/index.js
+++ components/Page/index.js
@@ -16,7 +16,6 @@ const Page = (props) => {
                 <meta property="og:type" content="website" />
                 <meta property="og:url" content="//miptbase.netlify.app/OpenGraph.jpg" />
                 <meta property="og:image" content="//miptbase.netlify.app/OpenGraph.jpg"/>
-                <script src="https://identity.netlify.com/v1/netlify-identity-widget.js"></script>
                 <script src="https://rest-api-test.tinkoff.ru/html/payForm/js/tinkoff_v2.js"></script>
                 <script src="https://www.paypal.com/sdk/js?currency=RUB&client-id=AWVifSid8kSj1W3ap0jqZNuhTX8Har9m_sdMWrfC0jh2vxwsex90gPoo0XpnCizDS5KOwv4cOlqgskbu"
                   id="paypal-sdk"></script>
