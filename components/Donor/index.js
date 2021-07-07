113d39aaf5053fb4e65c44add21b5f0b5bcef0a3
Татьяна Антоновна Черданцева <valehova.tany@gmail.com>
2021-07-07 12:11:36 +0300

    donors modal

--- components/Donor/index.js
+++ components/Donor/index.js
@@ -1,13 +1,69 @@
-import React from 'react'
+import React, {useState, useEffect} from 'react'
 import style from "./donor_.module.scss"
+import { useIsMobile } from '../../hooks/useIsMobile';
+import { makeStyles } from '@material-ui/core/styles';
+import Dialog from "@material-ui/core/Dialog";
+import DonorDetails from "../DonorDetails";
+import DialogTitle from "@material-ui/core/DialogTitle";
+import DialogContent from "@material-ui/core/DialogContent";
+import DialogActions from "@material-ui/core/DialogActions";
+
+
+const useStyles = makeStyles((theme) => ({
+    root: {
+        position: 'absolute',
+        background: 'rgba(0, 0, 0, 0.56)',
+    },
+    paper: {
+        borderRadius: '0.5rem',
+        overflow: 'hidden'
+    },
+    modalBody: {
+        width: '24.2rem',
+        height: '34.1rem',
+        background: '#2D83E8',
+        padding: '2.5rem 2rem',
+        display: 'flex',
+        flexDirection: 'column',
+        justifyContent: 'space-between'
+    }
+}));
+
 
 
 const Donor = (props) => {
-    const {name, description, img, company} = props;
+    const classes = useStyles();
+    const isMobile = useIsMobile();
+    const {name, description, img, company, text} = props;
+    const [modal, setModal] = React.useState(false);
+
     return (
-        <div className={style.donor}>
-            <div className={style.flipper}>
-                <div className={style.front}>
+        <>
+        {!isMobile
+            && (
+                <div className={style.donor} >
+                    <div className={style.flipper}>
+                        <div className={style.front}>
+                            <div className={style.img}>
+                                <img src={img} alt={name}/>
+                            </div>
+                            <div className={style.info}>
+                                <p className={style.name}>{name}</p>
+                                <p className={style.company}>{company}</p>
+                            </div>
+                        </div>
+                        <div className={style.back}>
+                            <p className={style['back-name']}>{name}</p>
+                            <p className={style['back-company']}>{company}</p>
+                            <p className={style['back-description']}>{description}</p>
+                        </div>
+                    </div>
+                </ div>
+            )}
+            {isMobile
+            && (
+                <>
+                <div className={style.donor} onClick={() => setModal(true)}>
                     <div className={style.img}>
                         <img src={img} alt={name}/>
                     </div>
@@ -16,13 +72,42 @@ const Donor = (props) => {
                         <p className={style.company}>{company}</p>
                     </div>
                 </div>
-                <div className={style.back}>
-                    <p className={style['back-name']}>{name}</p>
-                    <p className={style['back-company']}>{company}</p>
-                    <p className={style['back-description']}>{description}</p>
-                </div>
-            </div>
-        </div>
+                <Dialog
+                    classes={{
+                        root: classes.root,
+                        label: classes.label,
+                        paper: classes.paper
+                    }}
+                open={modal}
+                onClose={() => setModal(false)}
+                aria-labelledby="modal-slide-title"
+                aria-describedby="modal-slide-description"
+                >
+                    <DialogContent
+                        id="modal-slide-description"
+                        className={classes.modalBody}
+                    >
+                        <DonorDetails
+                            donor={name}
+                            company={company}
+                            text={text}
+                        />
+                        <button
+                            className={style.button}
+                            key="close"
+                            aria-label="Close"
+                            color="inherit"
+                            onClick={() => setModal(false)}
+                        >
+                            Назад
+                        </button>
+                    </DialogContent>
+
+                </Dialog>
+               </>
+            )}
+
+        </>
     )
 }
 
--- components/DonorsList/index.js
+++ components/DonorsList/index.js
@@ -1,24 +1,32 @@
-import React from "react";
+import React, { useState, useCallback } from "react";
 import style from "./donors-list_.module.scss"
+import { useIsMobile } from '../../hooks/useIsMobile';
 
 import Donor from "../Donor";
+import DonorDetails from "../DonorDetails";
+import Button from "../ui/Button";
 
 const DonorsList = (props) => {
+    const isMobile = useIsMobile();
     const {donors} = props;
+
     return (
         <div className={style['donors-list']}>
           <div className={style.list}>
               {donors.map((donor) => (
                   <Donor
                       key={donor.ID}
+                      donor={donor}
                       name={donor.Donor}
                       description= {`${donor.Department != "" &&  donor.Department != "-" ? donor.Department : ''} ${donor.Year != "" &&  donor.Year != "-" ? `(${donor.Year})` : ''}`}
                       // img={donor.img}
                       company={donor.Company}
+                      text={donor.Text}
                       img={`media/${donor.ID}.png`}
                   />
               ))}
           </div>
+
         </div>
     )
 }
