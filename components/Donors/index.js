a4f6a1a74e792b5fe7697dea220bda71985a8a98
Татьяна Антоновна Черданцева <valehova.tany@gmail.com>
2021-07-27 18:04:58 +0300

    console donors

--- components/Donors/index.js
+++ components/Donors/index.js
@@ -7,6 +7,8 @@ import DonorsList from "../DonorsList";
 
 const Donors = (props) => {
     const {donors, donorsPerson} = props;
+    console.log('donorsPerson', donorsPerson);
+    console.log('donorsPerson filter', donorsPerson.filter(donor => donor.Top === "1"));
     return (
         <section className={style.donors}>
             <div className={style.inner}>
