2766078b2c3a9352cf2b207b4fe9598d11813862
Татьяна Антоновна Черданцева <valehova.tany@gmail.com>
2021-08-09 20:37:11 +0300

    number icon

--- components/StandardDistribution/index.js
+++ components/StandardDistribution/index.js
@@ -1,5 +1,6 @@
 import React from "react";
 import style from "./standard-distribution_.module.scss";
+import cn from "classnames";
 
 const StandardDistribution = (props) => {
     const { distribution } = props;
@@ -9,8 +10,10 @@ const StandardDistribution = (props) => {
             <div className={style.items}>
                 {distribution.items.map((item, i) => (
                     <div className={style.item} key={i}>
-                        <div className={style['item-number']}>
-                            {i+1}
+                        <div className={cn(
+                            style['item-number'],
+                            style[`item-number_${i + 1}`]
+                        )} >
                         </div>
                         <div className={style['item-name']}>
                             {item.title}
