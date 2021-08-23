f2517cc2d363a791ee34d3d1f2e5126811575f80
Татьяна Антоновна Черданцева <valehova.tany@gmail.com>
2021-08-23 20:41:06 +0300

    https://github.com/miptbase/website/issues/5 share

--- components/SuccessComponent/index.js
+++ components/SuccessComponent/index.js
@@ -2,6 +2,28 @@ import React from "react";
 import style from "./success_.module.scss";
 import Image from "next/image";
 import SVG from 'react-inlinesvg';
+const shareUrl = `https://${process.env.NEXT_PUBLIC_DOMAIN}`;
+import {
+    EmailShareButton,
+    FacebookShareButton,
+    HatenaShareButton,
+    InstapaperShareButton,
+    LineShareButton,
+    LinkedinShareButton,
+    LivejournalShareButton,
+    MailruShareButton,
+    OKShareButton,
+    PinterestShareButton,
+    PocketShareButton,
+    RedditShareButton,
+    TelegramShareButton,
+    TumblrShareButton,
+    TwitterShareButton,
+    ViberShareButton,
+    VKShareButton,
+    WhatsappShareButton,
+    WorkplaceShareButton
+} from "react-share";
 
 const SuccessComponent = (props) => {
     const { content, image, share } = props;
@@ -35,16 +57,41 @@ const SuccessComponent = (props) => {
                         {share.title}
                     </h2>
                     <div className={style.items}>
-                        {share.items.map((item) => (
-                            <a className={style['share-link']} key={item.name} href={item.link} target='_blank' rel='noopener'>
-                                <SVG className={style['share-img']} src={`/${item.img}`} />
-                            </a>
-                        ))}
+                        <FacebookShareButton url={shareUrl}>
+                            <div className={style['share-link']}>
+                                <SVG className={style['share-img']} src={`/${share.items[0].img}`} />
+                            </div>
+                        </FacebookShareButton>
+                        <TelegramShareButton  url={shareUrl}>
+                            <div className={style['share-link']}>
+                                <SVG className={style['share-img']} src={`/${share.items[1].img}`} />
+                            </div>
+                        </TelegramShareButton>
+                        <div className={style['share-link']}>
+                            <SVG className={style['share-img']} src={`/${share.items[2].img}`} />
+                        </div>
+                        <VKShareButton  url={shareUrl}>
+                            <div className={style['share-link']}>
+                                <SVG className={style['share-img']} src={`/${share.items[3].img}`} />
+                            </div>
+                        </VKShareButton>
+                        <WhatsappShareButton url={shareUrl}>
+                            <div className={style['share-link']}>
+                                <SVG className={style['share-img']} src={`/${share.items[4].img}`} />
+                            </div>
+                        </WhatsappShareButton>
+                        <TwitterShareButton url={shareUrl}>
+                            <div className={style['share-link']}>
+                                <SVG className={style['share-img']} src={`/${share.items[5].img}`} />
+                            </div>
+                        </TwitterShareButton>
+                        <LinkedinShareButton url={shareUrl}>
+                            <div className={style['share-link']}>
+                                <SVG className={style['share-img']} src={`/${share.items[6].img}`} />
+                            </div>
+                        </LinkedinShareButton>
                     </div>
-
                 </div>
-
-
             </div>
         </section>
     )
