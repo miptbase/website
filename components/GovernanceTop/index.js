2562a58a379eb8721fcbbe8a4b85b24c0c559591
Татьяна Антоновна Черданцева <valehova.tany@gmail.com>
2021-08-23 16:59:31 +0300

    https://github.com/miptbase/website/issues/43 add text

--- components/GovernanceTop/index.js
+++ components/GovernanceTop/index.js
@@ -49,7 +49,6 @@ const CovernanceTop = (props) => {
                           }
                         </div>
                       ))}
-                      }
                     </>
                   )
                   }
