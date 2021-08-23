c5299702a6a77db2b7057c214858909ed0f9b7cb
Татьяна Антоновна Черданцева <valehova.tany@gmail.com>
2021-08-23 16:46:30 +0300

    https://github.com/miptbase/website/issues/43 reverse

--- components/GovernanceMain/index.js
+++ components/GovernanceMain/index.js
@@ -5,14 +5,22 @@ import Link from "next/link";
 import docImage from "../../public/media/document-icon.svg"
 import SVG from 'react-inlinesvg';
 import { report } from "../../content/report.js"
+import {useIsMobile} from "../../hooks/useIsMobile";
 
 const CovernanceMain = (props) => {
+    const isMobile = useIsMobile();
     const { board, fund, company } = props;
     const parseReport = useCallback((arr) =>{
         arr[0][0] = 'ЦК';
         const length = arr[0].filter(el => el).length;
          const newArr = arr.map((item) => item.slice(0, length));
          return newArr;
+    },[])
+    const parseReportMobile = useCallback((arr) =>{
+        arr[0][0] = 'ЦК';
+        const length = arr[0].filter(el => el).length;
+        const newArr = arr.map((item) => item.slice(0, length));
+        return newArr;
     },[])
     const sumArr = useCallback((arr) =>{
         return arr.map((item) => item.map((item) => item.replace('%', '')))
@@ -159,11 +167,24 @@ const CovernanceMain = (props) => {
                             <table className={style.table}>
                                 <tbody>
                                 <tr className={style.tr}>
-                                    {
-                                        parseReport(report)[0].map((item, i) => (
-                                          <th className={style.th} key={i}>{item}</th>
-                                        ))
+                                    { !isMobile && (
+                                      parseReport(report)[0].map((item, i) => (
+                                        <th className={style.th} key={i}>{item}</th>
+                                      ))
+                                    )
                                     }
+                                    { isMobile && (
+                                      <>
+                                          <th className={style.th}>{parseReport(report)[0][0]}</th>
+                                          <th className={style.th}>{parseReport(report)[0][1]}</th>
+                                          {
+                                            parseReport(report)[0].slice(2).reverse().map((item, i) => (
+                                            <th className={style.th} key={i}>{item}</th>
+                                          ))}
+                                      </>
+                                    )
+                                    }
+
                                 </tr>
                                 {
                                     parseReport(report).slice(1).map((row, i) => (
@@ -178,9 +199,24 @@ const CovernanceMain = (props) => {
                                                   {row[1]}
                                               </span>
                                           </td>
-                                          {row.slice(2).map((item, i) => (
-                                            <td className={style.td} key={i}>{item}</td>
-                                          ))}
+                                          {
+                                              !isMobile && (
+                                                <>
+                                                    {row.slice(2).map((item, i) => (
+                                                      <td className={style.td} key={i}>{item}</td>
+                                                    ))}
+                                                </>
+                                              )
+                                          }
+                                          {
+                                              isMobile && (
+                                                <>
+                                                    {row.slice(2).reverse().map((item, i) => (
+                                                      <td className={style.td} key={i}>{item}</td>
+                                                    ))}
+                                                </>
+                                              )
+                                          }
                                       </tr>
                                     ))
                                 }
@@ -194,13 +230,26 @@ const CovernanceMain = (props) => {
                                       </span>
                                     </td>
                                     {
-                                        sumArr(parseReport(report)).map((item, i) => (
-                                          <td className={style.td} key={i}>{item}%</td>
-                                          )
+                                        !isMobile && (
+                                          <>
+                                              {
+                                                  sumArr(parseReport(report)).map((item, i) => (
+                                                  <td className={style.td} key={i}>{item}%</td>
+                                                ))}
+                                          </>
+                                        )
+                                    }
+                                    {
+                                        isMobile && (
+                                          <>
+                                              {
+                                                  sumArr(parseReport(report)).reverse().map((item, i) => (
+                                                    <td className={style.td} key={i}>{item}%</td>
+                                                  ))}
+                                          </>
                                         )
                                     }
                                 </tr>
-
                                 </tbody>
                             </table>
                 </div>
