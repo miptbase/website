df9d0b87721c68153822936c9cc14d607a809eba
Татьяна Антоновна Черданцева <valehova.tany@gmail.com>
2021-09-06 19:58:03 +0300

    https://github.com/miptbase/website/issues/76 https://github.com/miptbase/website/issues/73 donors cards fix

--- components/DonorsHonorable/index.js
+++ components/DonorsHonorable/index.js
@@ -23,8 +23,8 @@ const DonorsHonorable = (props) => {
                <>
                    <div className={style['donors-top']}>
                        {donorsPerson.filter(donor =>
-                         donor['Name'] != ""
-                         ).sort((a, b) => a["Class"] > b["Class"] ? 1 : -1)
+                         (donor['Name'] != "" &&
+                         donor['Class'] === "A"))
                          .map((donor)  => (
                            <div className={style.item} key={donor.ID}>
                                <div className={style['donor-img']}>
@@ -47,6 +47,25 @@ const DonorsHonorable = (props) => {
                                        {donor['Name']}
                                    </div>
                                    <div className={style.text}>
+                                       <ReactMarkdown rehypePlugins={[rehypeRaw]} >
+                                           {donor['Description']}
+                                       </ReactMarkdown>
+                                   </div>
+                               </div>
+                           </div>
+                         ))}
+                   </div>
+                   <div className={style.donors}>
+                       {donorsPerson.filter(donor =>
+                         donor['Name'] !== "" && donor['Class'] === "B"
+                       )
+                         .map((donor)  => (
+                           <div className={style.item} key={donor.ID}>
+                               <div className={style.content}>
+                                   <div className={style.name}>
+                                       {donor['Name']}
+                                   </div>
+                                   <div className={style.text}>
                                        <ReactMarkdown rehypePlugins={[rehypeRaw]}>
                                            {donor['Description']}
                                        </ReactMarkdown>
@@ -62,8 +81,9 @@ const DonorsHonorable = (props) => {
                 <>
                     <div className={style['donors-top']}>
                         {donorsPerson.filter(donor =>
-                          donor['Name'] != ""
-                        ).sort((a, b) => a["Class"] > b["Class"] ? 1 : -1)
+                          donor['Name'] != "" &&
+                          donor['Class'] === "A"
+                        )
                           .slice(0, 9).map((donor)  => (
                             <div className={style.item} key={donor.ID}>
                                 <div className={style['donor-img']}>
@@ -97,53 +117,74 @@ const DonorsHonorable = (props) => {
                     </div>
                     {
                         !donorsVisible &&
-                          <div className={style['button-container']}>
-                              <Button
-                                color='blue'
-                                text={honorable.buttonText}
-                                width={'100'}
-                                buttonFunction={() => {setDonorsVisible(true)}}
-                              />
-                          </div>
+                        <div className={style['button-container']}>
+                            <Button
+                              color='blue'
+                              text={honorable.buttonText}
+                              width={'100'}
+                              buttonFunction={() => {setDonorsVisible(true)}}
+                            />
+                        </div>
                     }
                     {
                         donorsVisible &&
-                          <>
-                              <div className={style['donors-top']}>
-                                  {donorsPerson.filter(donor =>
-                                    donor['Name'] != ""
-                                  ).sort((a, b) => a["Class"] > b["Class"] ? 1 : -1)
-                                    .slice(10).map((donor)  => (
-                                      <div className={style.item} key={donor.ID}>
-                                          <div className={style['donor-img']}>
-                                              <Image
-                                                src={
-                                                    donorsImages.includes(`${donor['ID']}.png`) ?
-                                                      `/media/donors/${donor.ID}.png` : avatarMobile
-                                                }
-                                                alt={donor['Name']}
-                                                layout='fill'
-                                                objectFit='cover'
-                                                objectPosition='left center'
-                                                blurDataURL={`data:image/svg+xml;base64,${toBase64(avatarPlaceholder())}`}
-                                                placeholder="blur"
-                                                sizes='8vw'
-                                              />
-                                          </div>
-                                          <div className={style.content}>
-                                              <div className={style.name}>
-                                                  {donor['Name']}
-                                              </div>
-                                              <div className={style.text}>
-                                                  <ReactMarkdown rehypePlugins={[rehypeRaw]}>
-                                                      {donor['Description']}
-                                                  </ReactMarkdown>
-                                              </div>
-                                          </div>
-                                      </div>
-                                    ))}
-                              </div>
-                          </>
+                        <>
+                            <div className={style['donors-top']}>
+                                {donorsPerson.filter(donor =>
+                                  donor['Name'] !==  "" &&
+                                  donor['Class'] === "A"
+                                )
+                                  .slice(10).map((donor)  => (
+                                    <div className={style.item} key={donor.ID}>
+                                        <div className={style['donor-img']}>
+                                            <Image
+                                              src={
+                                                  donorsImages.includes(`${donor['ID']}.png`) ?
+                                                    `/media/donors/${donor.ID}.png` : avatarMobile
+                                              }
+                                              alt={donor['Name']}
+                                              layout='fill'
+                                              objectFit='cover'
+                                              objectPosition='left center'
+                                              blurDataURL={`data:image/svg+xml;base64,${toBase64(avatarPlaceholder())}`}
+                                              placeholder="blur"
+                                              sizes='8vw'
+                                            />
+                                        </div>
+                                        <div className={style.content}>
+                                            <div className={style.name}>
+                                                {donor['Name']}
+                                            </div>
+                                            <div className={style.text}>
+                                                <ReactMarkdown rehypePlugins={[rehypeRaw]}>
+                                                    {donor['Description']}
+                                                </ReactMarkdown>
+                                            </div>
+                                        </div>
+                                    </div>
+                                  ))}
+                            </div>
+                            <div className={style.donors}>
+                                {donorsPerson.filter(donor =>
+                                  donor['Class'] === "B"
+                                )
+                                  .map((donor)  => (
+                                    <div className={style.item} key={donor.ID}>
+                                        <div className={style['content-mini']}>
+                                            <div className={style.name}>
+                                                {donor['Name']}
+                                            </div>
+                                            <div className={style.text}>
+                                                <ReactMarkdown rehypePlugins={[rehypeRaw]}>
+                                                    {donor['Description']}
+                                                </ReactMarkdown>
+                                            </div>
+
+                                        </div>
+                                    </div>
+                                  ))}
+                            </div>
+                        </>
                     }
                 </>
             }
